9b0b7c1197670861174262fda6fc81717511a14f
Maanasa Panchakarla <manasapanchakarla25@gmail.com>
2025-12-26 20:17:02 +0000

    Update load_pdb.py

--- src/data_processing/load_pdb.py
+++ src/data_processing/load_pdb.py
@@ -2,3 +2,12 @@
 Tools for downloading and handling PDB structures
 for KRAS and related proteins.
 """
+def download_pdb(pdb_id:str)->str:
+  """
+    Download a PDB structure from the RCSB Protein Data Bank.
+    Args:
+        pdb_id (str): The PDB ID of the protein structure to download.
+    Returns:
+        str: The raw PDB file content as text.
+    """
+  pass
