78311993976aa09e1807f6bb4e21e50443ee3928
Maanasa Panchakarla <manasapanchakarla25@gmail.com>
2025-12-26 20:24:35 +0000

    Update load_pubchem.py

--- src/data_processing/load_pubchem.py
+++ src/data_processing/load_pubchem.py
@@ -1,1 +1,13 @@
-
+"""
+Tools for fetching molecular data from PubChem,
+including SMILES strings and compound properties.
+"""
+def get_smiles_from_cid(cid: int) -> str:
+    """
+    Retrieve the SMILES string for a compound using its PubChem CID.
+    Args:
+        cid (int): PubChem Compound ID.
+    Returns:
+        str: SMILES string for the compound.
+    """
+    pass
